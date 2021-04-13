b0c8caa7a124fe26331f3b0c18c78b6ec3839c7b
Emilio94 <emilio.994@gmail.com>
2021-04-13 11:15:50 +0200

    Header

--- javascript/main.js
+++ javascript/main.js
@@ -6,6 +6,38 @@ Vue.config.devtools = true;
 const app = new Vue({
     el : '#root',
     data : {
+        myImagesPath : 'imgs/',
+        images : {
+            header :'medical_logo_1x_light.png',
+            jumbotron : '/header-image-homepage.jpg',
+            footer : ''
+        },
+
+        nav_list: {
+            head : ['home', 'about', 'departments', 'articles']
+        },        
+
+        button_styles : {
+            empty : {
+                background : 'transparent',
+                borderRadius : '2px',
+            },
+            marine : {
+                background : '#39afbb',
+                borderRadius : '2px',
+                color : 'white'
+            }
+        },
+
+        button_tags : {
+            learn : 'LEARN MORE',
+            appointment : 'MAKE AN APPOINTMENT',
+            departments : 'VIEW OUR DEPARTMENTS'
+        }
+        
+
+
+
         
     }
 })
