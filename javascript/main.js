49f2c3ec9ba7291daaaeb4f6f23ced41083f450f
Emilio94 <emilio.994@gmail.com>
2021-04-13 12:15:03 +0200

    Footer

--- javascript/main.js
+++ javascript/main.js
@@ -7,14 +7,20 @@ const app = new Vue({
     el : '#root',
     data : {
         myImagesPath : 'imgs/',
+
         images : {
             header :'medical_logo_1x_light.png',
             jumbotron : '/header-image-homepage.jpg',
-            footer : ''
+            footer : 'medical_logo_1x_dark.png'
         },
 
-        nav_list: {
-            head : ['home', 'about', 'departments', 'articles']
+        nav_lists: {
+            head : ['home', 'about', 'departments', 'articles'],
+            footer : {
+                infos : ['121 King Street, London United Kingdom','1.800.458.556'],
+                departments : ['Cardiology', 'Pediatrics', 'Diabetes Care', 'Pre-natel Care','Ultrasound Echocardiogram'],
+                rights : ['© Copyright 2012-2020', 'Avada theme by', 'All Rights Reserved' ,'Powered by ']
+            }
         },        
 
         button_styles : {
@@ -33,7 +39,17 @@ const app = new Vue({
             learn : 'LEARN MORE',
             appointment : 'MAKE AN APPOINTMENT',
             departments : 'VIEW OUR DEPARTMENTS'
+        },
+        
+        font_awsome : ['fas fa-map-marker-alt', 'fas fa-phone', 'fas fa-angle-right'],
+
+        socials : {
+            classes : ['fb', 'tw', 'in', 'yt'],
+            icons : ['fab fa-facebook-f', 'fab fa-twitter', 'fab fa-instagram', 'fab fa-youtube']
         }
+
+
+
         
 
 
